290b72bbdff7e41bb7509271d8fe0e03c85613e0
OG-Mendez <135224145+OG-Mendez@users.noreply.github.com>
2024-09-19 18:10:50 +0100

    Update Streamlit_tutorial_1.py
    Added coded tom handle text input errors better

--- Streamlit_tutorial_1.py
+++ Streamlit_tutorial_1.py
@@ -4,11 +4,7 @@ import joblib
 from io import BytesIO
 import requests # or https
 
-# e.g. a file call stopwords saved by joblib
-# https://github.com/Proteusiq/hisia/v1.0.1/hisia/models/data/stops.pkl
-
-# change github.com to raw.githubusercontent.com
-
+# URL to model in my git repository 
 URI = "https://github.com/OG-Mendez/diabetes1-deployment/blob/main/diabetes_model_1.pkl?raw=true"
 
 class InvalidInputError(Exception):
@@ -79,28 +75,79 @@ def main():
     # the following lines create text boxes in which the user can enter
     # the data required to make the prediction
     Gender = st.selectbox("Gender", ("Male", "Female"), help="Select Gender")
-    AGE = st.text_input("Age", placeholder= "Type Here", help="Your Age")
+    AGE = st.text_input("Age", placeholder= "Type Here", help="Enter Age")
+    #The below code is for error handling
+    if AGE:
+        try:
+            number = float(AGE)  # Try converting input to a float
+            AGE = number
+        except ValueError:
+            st.error('Please enter a valid number.')
+    else:
+        st.write('Please enter a number.')
     HbA1c = st.text_input("HbA1c (Glycated Haemoglobin)", placeholder= "Type Here", help="Shows what your average "
                                                                                          "blood sugar (glucose) level "
                                                                                          "was over the past two to "
-                                                                                         "three months")
+                                                                                         "three months.")
+    if HbA1c:
+        try:
+            number = float(HbA1c)  # Try converting input to a float
+            HbA1c = number
+        except ValueError:
+            st.error('Please enter a valid number.')
+    else:
+        st.write('Please enter a number.')
     Chol = st.text_input("Cholestrol", placeholder="Type Here", help="If you have too much cholesterol in your blood, "
                                                                      "it can combine with other substances in the blood"
                                                                      " to form plaque. Plaque sticks to the walls of "
                                                                      "your arteries.")
+    if Chol:
+        try:
+            number = float(Chol)  # Try converting input to a float
+            Chol = number
+        except ValueError:
+            st.error('Please enter a valid number.')
+    else:
+        st.write('Please enter a number.')
     TG = st.text_input("TG (TriGlycerides)", placeholder="Type Here", help="Triglycerides come from the food you "
                                                                            "eat. Extra calories are turned into "
                                                                            "triglycerides and stored in fat cells "
                                                                            "for later use.")
+    if TG:
+        try:
+            number = float(TG)  # Try converting input to a float
+            TG = number
+        except ValueError:
+            st.error('Please enter a valid number.')
+    else:
+        st.write('Please enter a number.')
     VLDL = st.text_input("VLDL (Very Low Density Lipoprotein)", placeholder="Type Here", help="VLDL is a type of bad "
                                                                                               "cholesterol because it "
                                                                                               "helps cholesterol build "
                                                                                               "up on the walls of "
                                                                                               "arteries.")
+    if VLDL:
+        try:
+            number = float(VLDL)  # Try converting input to a float
+            VLDL = number
+        except ValueError:
+            st.error('Please enter a valid number.')
+    else:
+        st.write('Please enter a number.')
     BMI = st.text_input("BMI (Body Mass Index)", placeholder="Type Here", help="Body mass index (BMI) is a medical "
                                                                                "screening tool that measures the ratio "
                                                                                "of your height to your weight to estimate "
                                                                                "the amount of body fat you have.")
+    if BMI:
+        try:
+            number = float(BMI)  # Try converting input to a float
+            BMI = number
+        except ValueError:
+            st.error('Please enter a valid number.')
+    else:
+        st.write('Please enter a number.')
+
+
 
     result = ""
     # the below line ensures that when the button called 'Predict' is clicked,
